00eb5cf86b96810442def031dc8c550f2546c6a2
Danyelle <dkobes15@gmail.com>
2025-03-11 20:07:42 -0500

    working on cypress

---
+++ cypress/e2e/test.js
@@ -0,0 +1,6 @@
+// describe('Hello World Test', () => {
+//     it('Visits the app', () => {
+//       cy.visit('http://localhost:3001'); // Adjust the URL to match your app's URL
+//       cy.contains('Hello, world!'); // Adjust the text to match something on your app's page
+//     });
+//   });
